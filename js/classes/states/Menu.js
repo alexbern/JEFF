e0a060c218a676014e6bca22a8ee103ec9c6e3eb
Alex Bernard <itsalexbernard@gmail.com>
2016-11-30 20:24:17 +0100

    fixed a bug with added function

--- js/classes/states/Menu.js
+++ js/classes/states/Menu.js
@@ -3,16 +3,15 @@ const board = new five.Board();
 const sensorpoint = require('../../utils/sensorpoint');
 
 class Menu{
-  create(){
-
+  preload(){
     board.on('ready', () => {
-      let stuff = sensorpoint("A0", "580");
-
+      let control1 = sensorpoint("A0", 580);
       this.sprite = this.game.add.sprite(100,100,'sprite');
       this.sprite.anchor.setTo(0.5, 0.5);
     });
   }
   update(){
+
   }
   stuff(){
     console.log('do stuff');
--- js/utils/sensorpoint.js
+++ js/utils/sensorpoint.js
@@ -6,9 +6,11 @@ module.exports = (pin, sens) => {
 
   sensor.on("change", (value) => {
     if (value > sens) {
-      console.log(pin + ' is unactive');
+      console.log(pin + ' is active with sensitivity of ' + sens);
+      return 1;
     }else{
-      console.log(pin + ' is active');
+      console.log(pin + ' is unactive with sensitivity of ' + sens);
+      return 0;
     }
   });
 
