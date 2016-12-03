f79adb59a0942f872e2cfa71bdd2334593c9b60a
Alex Bernard <itsalexbernard@gmail.com>
2016-12-03 17:36:16 +0100

    added global cooldown for awnsers

--- js/classes/states/Play.js
+++ js/classes/states/Play.js
@@ -4,23 +4,32 @@ class Play{
     this.initControls();
     this.timerTime = 3;
     this.enableControls = 0;
+    this.enableAwnser = 0;
     this.countdown();
   }
   startGame(){
     console.log('Game Start');
   }
   update(){
-    if (this.key1.isDown && this.enableControls != 0) {
+    if (this.key1.isDown && this.enableControls != 0 && this.enableAwnser != 0) {
       console.log('ONE');
+      this.scoreUp(this.player1);
     }else if(this.key2.isDown && this.enableControls != 0){
       console.log('TWO');
     }
-    else if(this.key3.isDown && this.enableControls != 0){
+    else if(this.key3.isDown && this.enableControls != 0 && this.enableAwnser != 0){
+      this.scoreUp(this.player2);
       console.log('THREE');
     }
     else if(this.key4.isDown && this.enableControls != 0){
       console.log('FOUR');
     }
+  }
+  scoreUp(player){
+    console.log(player);
+    player.x += 20;
+    this.enableAwnser = 0;
+    this.awnserCooldown();
   }
   initPlayers(){
     this.player1 = this.game.add.sprite(200, 200, 'sprite');
@@ -42,9 +51,21 @@ class Play{
       }else{
         this.startGame();
         this.enableControls = 1;
+        this.enableAwnser = 1;
         clearInterval(countdown);
       }
     }, 1000);
+  }
+  awnserCooldown(){
+    let cooldownTimer = 2;
+    let cooldown = setInterval(()=>{
+      if (cooldownTimer > 0) {
+        cooldownTimer--;
+      }else{
+        this.enableAwnser = 1;
+        clearInterval(cooldown);
+      }
+    }, 1000)
   }
 }
 
